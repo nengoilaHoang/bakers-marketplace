d57e1ab7649f2bb4c227e26ed3b649c2b7f7a26a
SAPAAAA <buithoanhtu2004@gmail.com>
2026-09-14 22:14:30 +0700

    feat: implement zod in be to design models, and validate data

---
+++ be/src/models/images.model.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+export const ImageTableSchema = z.object({
+	id: z.uuidv4(),
+	displayName: z.string(),
+	originalName: z.string(),
+	url: z.string(),
+	createdAt: z.date().default(() => new Date()),
+});
+
+export const ImageSchema = ImageTableSchema;
+
+export type ImageRow = z.infer<typeof ImageTableSchema>;
+export type Image = z.infer<typeof ImageSchema>;
