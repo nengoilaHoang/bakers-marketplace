b117b2fef55f3e95abb2f9d78dcc19fc6671f1c3
SAPAAAA <buithoanhtu2004@gmail.com>
2026-09-23 20:09:51 +0700

    fix: update next config

--- fe/next.config.ts
+++ fe/next.config.ts
@@ -1,9 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:4000/api';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-  reactStrictMode: true,
+	/* config options here */
+	reactCompiler: true,
+	reactStrictMode: true,
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `${BASE_URL}/:path*`,
+			},
+		];
+	},
 };
 
 export default nextConfig;
